0b62ba538f849640abc8cf66b5891ad9bf4f6f17
Joseigarcia <joseigarcia@users.noreply.github.com>
2026-07-02 06:44:22 -0600

    Initial commit: Project setup for Aristastudio

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+﻿// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  image: {
+    service: { entrypoint: 'astro/assets/services/sharp' }
+  }
+});
