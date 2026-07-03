93b9ce13177f55f3a40cf1443e4bd2e3413e9084
Joseigarcia <joseigarcia@users.noreply.github.com>
2026-07-02 18:47:27 -0600

    Configure custom domain for GitHub Pages (aristastudio.com.mx)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://aristastudio.com.mx',
   output: 'static',
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' }
